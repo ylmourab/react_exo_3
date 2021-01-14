100c710a5c302739aaa1e8851e97ab4292063611
Younes <ylmourab77@gmail.com>
2021-01-14 11:42:30 +0100

    fin exercice 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Portfolio</li>
+        </ul>
+    )
+}
+
+export default NavBar
