a99eb8f463fd0ca358f1ea6de7c81e007cafd001
Younes <ylmourab77@gmail.com>
2021-01-14 11:34:33 +0100

    modif component app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import './style.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Exo3</h1>
+      <img src="https://www.google.com/search?q=image&sxsrf=ALeKk009Z7Fpmz2N7_XqSc9ol6OT7eGLkQ:1610619974680&tbm=isch&source=iu&ictx=1&fir=_2JirDBiGzi3lM%252CQ927BxHsPbDVhM%252C_&vet=1&usg=AI4_-kQjWmp5_S7FlDg8yrD5QBMwcqla2Q&sa=X&ved=2ahUKEwiJyNX7mpvuAhXwwAIHHdpeCVgQ9QF6BAgPEAE#imgrc=_2JirDBiGzi3lM" alt=""/>
       
     </div>
   );
